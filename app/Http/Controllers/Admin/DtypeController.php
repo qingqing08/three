d59d2ec3abd68746004e2d05d2d39864d60ff903
Your Name <you@example.com>
2018-08-22 18:32:35 +0800

    zhuang

--- app/Http/Controllers/Admin/DtypeController.php
+++ app/Http/Controllers/Admin/DtypeController.php
@@ -4,8 +4,44 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\DB;
 
 class DtypeController extends Controller
 {
-    //
+
+    //跟单类型添加
+    public function dtype_add(){
+       return view('admin.dtype.add',['title' => '跟单类型添加']);
+    }
+
+    //执行类型添加
+    public function dtype_add_do(){
+        //接收post值
+        $name = input::post('name');
+        $status = input::post('status');
+        $c_time = time();
+
+        $arr = [
+            'name' => $name,
+            'c_time' => $c_time,
+            'status' => $status
+        ];
+
+        $res = DB::table('d_type') -> insert($arr);
+
+        if($res){
+            return (['msg' => '添加成功' , 'code' => 1]);
+        }else{
+            return (['msg' => '添加失败' , 'code' => 2]);
+        }
+    }
+
+    //跟单类型列表
+    public function dtype_list(){
+
+        $list = DB::table('d_type') -> get();
+
+        return view('admin.dtype.list' , ['title' => '跟单类型展示']) -> with('list',$list);
+    }
 }
---
+++ resources/views/admin/dtype/add.blade.php
@@ -0,0 +1,89 @@
+{{--加载头部代码--}}
+@include('layouts.header')
+<body>
+<div class="x-body">
+    <form class="layui-form">
+
+        <div class="layui-form-item">
+            @csrf
+            <label for="username" class="layui-form-label">
+                <span class="x-red">*</span>类型名称
+            </label>
+            <div class="layui-input-inline">
+                <input type="text" id="rule_name" name="name" required="" autocomplete="off" class="layui-input">
+            </div>
+            <div class="layui-form-mid layui-word-aux">
+                <span class="x-red">*</span>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">是否显示</label>
+            <div class="layui-input-block">
+                <input type="checkbox" name="status" lay-skin="switch" value='1' lay-text="显示|隐藏" checked>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label for="L_repass" class="layui-form-label">
+            </label>
+            <button  class="layui-btn" lay-filter="add" lay-submit="">
+                增加
+            </button>
+        </div>
+    </form>
+</div>
+<script>
+    layui.use(['form','layer'], function(){
+        $ = layui.jquery;
+        var form = layui.form
+            ,layer = layui.layer;
+
+        //监听提交
+        form.on('submit(add)', function(data){
+            var check = $("input[type='checkbox']").prop("checked");
+            if(check == true){
+                var status = 1;
+            }else {
+                var status = 0;
+            }
+            //alert(status);
+            // alert(parent_id);
+            var name = $("input[name=name]").val();
+            var token = $("input[name=_token]").val();
+            //alert(name)
+            $.ajax({
+                url:"dtype-add-do",
+                type:"post",
+                dataType:"json",
+                data:{
+                    'status':status,
+                    'name':name,
+                    '_token':token,
+                },
+                cache:false,
+                async:false,
+                success:function (data){
+                    if (data.code == 1) {
+                        layer.msg(data.msg, {icon: data.code, time: 1500}, function () {
+                            location.href = "dtype-add";
+                        });
+                    } else {
+                        layer.msg(data.msg, {icon: data.code});
+                    }
+
+                }
+            })
+            return false;
+        });
+
+
+    });
+</script>
+<script>var _hmt = _hmt || []; (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?b393d153aeb26b46e9431fabaf0f6190";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();</script>
+</body>
+
+</html>
