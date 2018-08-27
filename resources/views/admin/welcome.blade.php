2adabb07af25b47979ea72006259f94fc7bde0fa
Your Name <you@example.com>
2018-08-27 16:42:47 +0800

    zhuang

--- resources/views/admin/welcome.blade.php
+++ resources/views/admin/welcome.blade.php
@@ -13,7 +13,7 @@
     <body>
     <div class="x-body layui-anim layui-anim-up">
         <blockquote class="layui-elem-quote">欢迎管理员：
-            <span class="x-red">{{$staff_info->name}}</span>！当前时间:<?php echo date('Y-m-d H:i:s');?></blockquote>
+            <span class="x-red">{{$staff_info->name}}</span>！当前时间: <span style="color:red;" id="show"></span></blockquote>
         <fieldset class="layui-elem-field">
             <legend>数据统计</legend>
             <div class="layui-field-box">
@@ -129,5 +129,46 @@
           s.parentNode.insertBefore(hm, s);
         })();
         </script>
+
+    <script>
+
+
+        function run(){
+            var time = new Date();//获取系统当前时间
+            var year = time.getFullYear();
+            var month = time.getMonth()+1;
+            var date= time.getDate();//系统时间月份中的日
+            var day = time.getDay();//系统时间中的星期值
+            var weeks = ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
+            var week = weeks[day];//显示为星期几
+            var hour = time.getHours();
+            var minutes = time.getMinutes();
+            var seconds = time.getSeconds();
+            console.log(seconds);
+            if(month<10){
+                month = "0"+month;
+            }
+            if(date<10){
+                date = "0"+date;
+            }
+            if(hour<10){
+                hour = "0"+hour;
+            }
+            if(minutes<10){
+                minutes = "0"+minutes;
+            }
+            if(seconds<10){
+                seconds = "0"+seconds;
+            }
+            //var newDate = year+"年"+month+"月"+date+"日"+week+hour+":"+minutes+":"+seconds;
+            document.getElementById("show").innerHTML = year+"-"+month+"-"+date+" "+week+hour+":"+minutes+":"+seconds;
+            setTimeout('run()',1000);
+        }
+
+        run();
+
+
+
+    </script>
     </body>
 </html>
